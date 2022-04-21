63d373f07c747af483a7df9e51411ed06a625d95
Chris McGee <chris@chrismcgee.info>
2022-04-21 19:48:53 -0400

    🗃️ Refines TV episode selection

--- queries/episodes.sql
+++ queries/episodes.sql
@@ -1,5 +1,6 @@
-SELECT substr('JanFebMarAprMayJunJulAugSepOctNovDec', 1 + 3*strftime('%m', mi.added_at), -3) AS amonth, strftime('%d%Y-%H:%M', mi.added_at, 'localtime') AS adatetime, mi.title show_title, mip.title season_title, mig.title series_title
+SELECT substr('JanFebMarAprMayJunJulAugSepOctNovDec', 1 + 3*strftime('%m', mi.added_at, 'localtime'), -3) || ' ' || strftime('%d, %Y, %H:%M', mi.added_at, 'localtime') AS adatetime, mi.title show_title, mip.title season_title, mig.title series_title, mi.year, m.width || 'x' || m.height AS res
 FROM metadata_items mi
 JOIN metadata_items mip ON mip.id = mi.parent_id
 JOIN metadata_items mig ON mig.id = mip.parent_id
+JOIN media_items m on mi.id = m.metadata_item_id
 WHERE mi.library_section_id = 2;
