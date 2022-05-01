5abddcfbca703304220e2a9e740f94606e53e337
Chris McGee <chris@chrismcgee.info>
2022-05-01 16:59:58 -0400

    🗃️ Adjusts optional query clauses

--- queries/movies_viewed.sql
+++ queries/movies_viewed.sql
@@ -2,4 +2,4 @@ SELECT substr('JanFebMarAprMayJunJulAugSepOctNovDec', 1 + 3*strftime('%m', miv.v
 FROM metadata_item_views miv
 JOIN metadata_items mi ON mi.guid = miv.guid
 WHERE miv.id > 805 AND miv.account_id = 1 AND miv.library_section_id = 1
--- AND miv.title LIKE '%48%';
+-- AND miv.title LIKE '%partial_movie_title%';
